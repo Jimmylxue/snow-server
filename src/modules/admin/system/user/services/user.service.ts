49a7c004e20159eb15eb9d0f40bb91d26e1f028c
jimmyxuexue <1002661758@qq.com>
2024-12-16 10:54:14 +0800

    随机密码修改为纯数字

--- src/modules/admin/system/user/services/user.service.ts
+++ src/modules/admin/system/user/services/user.service.ts
@@ -274,8 +274,7 @@ export class UserService {
   }
 
   generateUserNameNonceStr() {
-    const chars =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const chars = '0123456789';
     let nonceStr = '';
     for (let i = 0; i < 8; i++) {
       nonceStr += chars.charAt(Math.floor(Math.random() * chars.length));
