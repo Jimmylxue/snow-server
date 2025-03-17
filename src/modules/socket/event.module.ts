eac0da491afb20006039a0d4bbdff31e0f09a986
jimmyxuexue <1002661758@qq.com>
2025-03-17 14:51:46 +0800

    去除socket

--- src/modules/socket/event.module.ts
+++ src/modules/socket/event.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { EventsGateway } from './gateway';
 import { JwtAuthGuard } from './core/jwt-auth.guard';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [],
-  providers: [EventsGateway, JwtService, JwtAuthGuard],
+  providers: [JwtService, JwtAuthGuard],
 })
 export class EventsModule {}
